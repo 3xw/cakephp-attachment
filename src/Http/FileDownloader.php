1cb6969e8a162f9caa54a4209d00011b7391c091
awallef <antoine.wallef@gmail.com>
2021-09-24 10:02:57 +0200

    Create FileDownloader.php

---
+++ src/Http/FileDownloader.php
@@ -0,0 +1,60 @@
+<?php
+namespace Trois\Attachment\Http;
+
+use const UPLOAD_ERR_OK;
+
+use Zend\Diactoros\StreamFactory;
+use Zend\Diactoros\UploadedFileFactory;
+
+class FileDownloader
+{
+  static public $streamFactory = null;
+
+  static public $uploadFileFactory = null;
+
+  static public function getStreamFactory(): StreamFactory
+  {
+    if(!self::$streamFactory) self::$streamFactory = new StreamFactory();
+    return self::$streamFactory;
+  }
+
+  static public function getUploadedFileFactory(): UploadedFileFactory
+  {
+    if(!self::$uploadFileFactory) self::$uploadFileFactory = new UploadedFileFactory();
+    return self::$uploadFileFactory;
+  }
+
+  static public function urlToFormArray(string $url, string $profile = 'default'): array
+  {
+    // check url
+    if(empty($url)) return self::createFormArray($profile);
+
+    // get name
+    $parts = explode('/',$url);
+    $name = array_pop($parts);
+
+    // get headers
+    $headers = get_headers($url,1);
+
+    // check status
+    if(substr($headers[0], 9, 3) != 200) return self::createFormArray($profile, $name);
+
+    // download & return array
+    return self::createFormArray($profile, $name, $headers['Content-Type'], file_get_contents($url));
+  }
+
+  static public function createFormArray(string $profile = 'default', string $name = '', string $clientMediaType = null, string $content = ''): array
+  {
+    // create stream
+    $stream = self::getStreamFactory()->createStream($content);
+
+    // build status
+    $status = $stream->getSize()? UPLOAD_ERR_OK: UPLOAD_ERR_NO_FILE;
+
+    // create uploadFile
+    $uploadFile = self::getUploadedFileFactory()->createUploadedFile($stream, null, $status, $name, $clientMediaType);
+
+    // return array
+    return ['name' => $name, 'profile' => $profile, 'path' => $uploadFile];
+  }
+}
