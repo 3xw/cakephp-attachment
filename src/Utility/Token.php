bf801ed2a403d09492d0d01e7451099c91f0ba7a
jeanvier <damien.grossfeld@gmail.com>
2026-01-29 15:29:53 +0100

    feat: Update JWT library and adapt Token utility for v7

--- src/Utility/Token.php
+++ src/Utility/Token.php
@@ -2,27 +2,27 @@
 namespace Trois\Attachment\Utility;
 
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Cake\Utility\Security;
 
 class Token
 {
-
   public static function encode($obj)
   {
-    return JWT::encode($obj, self::key());
+    // v6/v7: encode(payload, key, alg)
+    return JWT::encode((array) $obj, self::key(), 'HS256');
   }
 
   public static function decode($cypher)
   {
-    try {
-      return JWT::decode($cypher, self::key(),['HS256']);
-    } catch (\Exception $e) {
-      throw $e;
-    }
+    // v6/v7: decode(jwt, Key|Key[])
+    return JWT::decode($cypher, new Key(self::key(), 'HS256'));
   }
 
   public static function key()
   {
-    return Security::getSalt();
+    // IMPORTANT: en v7 il peut y avoir une validation de longueur de clé.
+    // Security::getSalt() doit être suffisamment long et aléatoire.
+    return (string) Security::getSalt();
   }
 }
